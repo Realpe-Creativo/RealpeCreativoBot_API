aa20dfdbf54874c0fb573b49de20d318731cda73
Santiago Jiménez <santiago.abihatar.jimenez@correounivalle.edu.co>
2025-08-15 17:32:23 -0500

    Modifying querys.js

--- src/utilities/schemas/ClientSchema.js
+++ src/utilities/schemas/ClientSchema.js
@@ -15,26 +15,7 @@ const ClientSchema = Object.freeze({
         document_type: Joi.string().max(3).required(),
         document_number: Joi.string().max(20).required(),
         email: Joi.string().max(255).email().required(),
-        cellphone_number: Joi.string().max(15).required(),
-        guardian_name: Joi.string().allow('').max(200),
-        date_of_birth: Joi.string()
-            .pattern(dateRegex)
-            .required()
-            .custom((value, helpers) => {
-                const validDate = dayjs(value, 'DD/MM/YYYY', true).isValid();
-                if (!validDate) {
-                    return helpers.error('date.invalid'); // disparamos un error personalizado
-                }
-                return value;
-            })
-            .messages({
-                'string.pattern.base': 'The date of birth must be in the format DD/MM/YYYY.',
-                'date.invalid': 'The date of birth must be a valid date.'
-            }),
-        neighborhood: Joi.string().max(100).required(),
-        address: Joi.string().max(100).required(),
-        sent_by_institution: Joi.bool().required(),
-        institution: Joi.string().allow('').max(200)
+        cellphone_number: Joi.string().max(15).required()
     }),
     UPDATE_CLIENT_SCHEMA: Joi.object({
         id: Joi.string().pattern(/^\d{1,8}$/).required(),
@@ -43,26 +24,7 @@ const ClientSchema = Object.freeze({
         names: Joi.string().max(100).required(),
         last_names: Joi.string().max(100).required(),
         email: Joi.string().max(255).email().required(),
-        cellphone_number: Joi.string().max(15).required(),
-        guardian_name: Joi.string().allow('').max(200),
-        date_of_birth: Joi.string()
-            .pattern(dateRegex)
-            .required()
-            .custom((value, helpers) => {
-                const validDate = dayjs(value, 'DD/MM/YYYY', true).isValid();
-                if (!validDate) {
-                    return helpers.error('date.invalid'); // disparamos un error personalizado
-                }
-                return value;
-            })
-            .messages({
-                'string.pattern.base': 'The date of birth must be in the format DD/MM/YYYY.',
-                'date.invalid': 'The date of birth must be a valid date.'
-            }),
-        neighborhood: Joi.string().max(100).required(),
-        address: Joi.string().max(100).required(),
-        sent_by_institution: Joi.bool().required(),
-        institution: Joi.string().allow('').max(200)
+        cellphone_number: Joi.string().max(15).required()
     })
 });
 
